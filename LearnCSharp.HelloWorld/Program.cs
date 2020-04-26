26634269f7725e530d4c31c944c55cb1b60ff315
Prolifik Lexzy <pro_lifik@live.com>
2020-04-26 09:27:07 +0100

    [Prolifik]

--- LearnCSharp.HelloWorld/Program.cs
+++ LearnCSharp.HelloWorld/Program.cs
@@ -6,10 +6,32 @@ namespace LearnCSharp.HelloWorld
     {
         static void Main()
         {
+            var microwave = new Microwave<int>();
+            var themostat = new Themostat();
+            microwave.OnTemperatureChanged += themostat.TemperatureChangeHandler;
+            microwave.OnTemperatureChanged += (int temp) =>
+            {
+                Console.WriteLine("Stopping heater temperature by lamda expression");
+            };
 
-            Console.WriteLine("");
+            //microwave.OnTemperatureChanged(100);
+
+            Console.Read();
         }
     }
 
-    public class 
+    public class Themostat
+    {
+        public void TemperatureChangeHandler(int temperature)
+        {
+            Console.WriteLine($"Stopping heater temperature is at {temperature} degrees by themostat.");
+        }
+    }
+
+    public class Microwave<T>
+    {
+        public event TemperatureChanged<T> OnTemperatureChanged;
+    }
+
+    public delegate void TemperatureChanged<T>(T temperature);
 }
